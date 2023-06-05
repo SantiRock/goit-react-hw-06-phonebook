8502a892919570bf072dfe97d5bb55e5cbe6ed3d
Santiago Quintero <santiago.qp86@gmail.com>
2023-06-05 13:30:37 -0500

    version2 - Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import { contactsReducers } from "./sliceContacts";
 import { filterReducer } from "./sliceFilter";
 
-export const store = configureStore({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['contacts'],
+}
+
+const rootReducer = combineReducers({
+    filter: filterReducer,
+    contacts: contactsReducers,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
+
+export const store =  configureStore({
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+
+/*
+export default () => {
+    let store = configureStore({
+        reducer: {
+            contacts: persistedReducer,
+            filter: filterReducer,
+        }
+    })
+    let persistor = persistStore(store)
+    return { store, persistor }
+}
+
+const store = configureStore({
     reducer: {
         contacts: contactsReducers,
         filter: filterReducer,
     }
-})
+})*/
